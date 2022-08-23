48002d06284fb02144b4b57a0c8fb500717bf200
SmileStrikeS <domincelr@gmail.com>
2022-08-23 10:05:04 +0800

    Initial Commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,11 +4,11 @@ import "../sass/app.scss";
 
 import * as bootstrap from "bootstrap";
 
-require("./bootstrap");
-import "particles.js/particles";
-const particlesJS = window.particlesJS;
+// require("./bootstrap");
+// import "particles.js/particles";
+// const particlesJS = window.particlesJS;
 
-// JSON file is located in the directory of 'public/js/particlejs-config.json'
-particlesJS.load("particles-js", "js/particlesjs.json", function () {
-    console.log("callback - particles.js config loaded");
-});
+// // JSON file is located in the directory of 'public/js/particlejs-config.json'
+// particlesJS.load("particles-js", "js/particlesjs.json", function () {
+//     console.log("callback - particles.js config loaded");
+// });
